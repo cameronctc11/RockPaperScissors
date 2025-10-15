411bd69fb4d62b884b001ac97f548cd47fc01cd3
cameronmckenzie <cameronctc11@gmail.com>
2025-10-15 14:22:57 -0600

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by McKenzie, Cameron - Student on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
